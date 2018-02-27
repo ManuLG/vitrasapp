c8b48a72123436178a3a29af1250cdcb8bd48319
mlorenzo <mlorenzogandara@gmail.com>
2018-02-27 10:55:00 +0100

    Test request

--- VitrasApp/app/src/main/java/com/mlorenzo/vitrasapp/MyCardAdapter.java
+++ VitrasApp/app/src/main/java/com/mlorenzo/vitrasapp/MyCardAdapter.java
@@ -1,5 +1,6 @@
 package com.mlorenzo.vitrasapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -7,6 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 import java.util.List;
 
@@ -51,11 +60,36 @@ public class MyCardAdapter extends RecyclerView.Adapter<MyCardAdapter.ContactVie
             v.setOnClickListener(new View.OnClickListener() {
                 @Override public void onClick(View v) {
                     Log.d("DDD", vName.getText().toString());
-                    Intent goToNextActivity = new Intent(v.getContext(), StopDetailActivity.class);
-                    goToNextActivity.putExtra("name", vName.getText().toString());
-                    v.getContext().startActivity(goToNextActivity);
+
+                    testRequest(v.getContext());
                 }
             });
+        }
+
+        public void testRequest(final Context context) {
+
+            String url = "http://manulg.ddns.net:5000/stops/6660/estimates";
+
+            JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                    (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.d("Request", response.toString());
+
+                            Intent goToNextActivity = new Intent(context, StopDetailActivity.class);
+                            goToNextActivity.putExtra("name", response.toString());
+                            context.startActivity(goToNextActivity);
+                        }
+                    }, new Response.ErrorListener() {
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // TODO Auto-generated method stub
+
+                        }
+                    });
+            Volley.newRequestQueue(context).add(jsObjRequest);
         }
     }
 }
