33f26e97e501464d88fb41baf6d26144d071c585
mlorenzo <mlorenzogandara@gmail.com>
2018-03-01 17:21:56 +0100

    Minimal visual changes

--- VitrasApp/app/src/main/java/com/mlorenzo/vitrasapp/stopDetail/StopDetailActivity.java
+++ VitrasApp/app/src/main/java/com/mlorenzo/vitrasapp/stopDetail/StopDetailActivity.java
@@ -4,16 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.widget.TextView;
 
-import com.mlorenzo.vitrasapp.MyCardAdapter;
 import com.mlorenzo.vitrasapp.R;
-import com.mlorenzo.vitrasapp.StopInformation;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -29,7 +23,6 @@ import java.util.List;
 public class StopDetailActivity extends AppCompatActivity {
 
     MyEstimatesAdapter ca;
-    List<EstimateInformation> infor = new ArrayList();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,14 +30,10 @@ public class StopDetailActivity extends AppCompatActivity {
         setContentView(R.layout.stop_detail);
 
         Intent intent= getIntent();
-
-        TextView text = findViewById(R.id.text);
-
-        String debug = intent.getStringExtra("response");
-        Log.d("TEST", debug);
+        String response = intent.getStringExtra("response");
 
         try {
-            JSONObject json = new JSONObject(debug);
+            JSONObject json = new JSONObject(response);
 
             RecyclerView recList = findViewById(R.id.cardList);
             recList.setHasFixedSize(true);
@@ -52,11 +41,6 @@ public class StopDetailActivity extends AppCompatActivity {
             LinearLayoutManager llm = new LinearLayoutManager(this);
             llm.setOrientation(LinearLayoutManager.VERTICAL);
             recList.setLayoutManager(llm);
-
-            RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2);
-            recList.setLayoutManager(mLayoutManager);
-//            recList.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
-            recList.setItemAnimator(new DefaultItemAnimator());
 
             ca = new MyEstimatesAdapter(parseResponse(json));
             recList.setAdapter(ca);
@@ -66,8 +50,8 @@ public class StopDetailActivity extends AppCompatActivity {
 
     }
 
-    private List parseResponse(JSONObject response) {
-        List<EstimateInformation> list = new ArrayList<EstimateInformation>();
+    private List<EstimateInformation> parseResponse(JSONObject response) {
+        List<EstimateInformation> list = new ArrayList<>();
 
         try {
             JSONArray array = response.getJSONArray("buses");
